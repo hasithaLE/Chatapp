fbdd2fdf9e9e363d52435646ca59680a74ffbf44
Hasitha Edirisinghe <hasithale@gmail.com>
2023-10-09 23:57:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("ChatBot 1.0");
+		userInput();
+	}
+
+	static void userInput()
+	{
+		Console.WriteLine("Hi - to Customer Support \nBuy - to show products \nBye - to End Chat\n");
+		Console.Write("You >> ");
+		string msg = Console.ReadLine();
+
+		if (msg == "Hi") 
+		{
+			Console.WriteLine("Hey, Welcome");
+			userInput();
+		}
+		else if (msg == "Buy")
+		{
+			Console.WriteLine("Welcome to shop");
+			userInput();
+		}
+		else if (msg == "Bye")
+		{
+			Console.WriteLine("See you...");
+		}
+		else
+		{
+			Console.WriteLine("Invalid Input");
+			userInput();
+		}
+		//Console.WriteLine("Your message is : " + msg);
+	}
+}
